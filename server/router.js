ff5cde6f64e9c7a0d4caa7cb04e1a60443023691
Christian Ingram <clingram1@aol.com>
2017-09-18 16:05:31 -0400

    Added API

--- server/router.js
+++ server/router.js
@@ -1,4 +1,26 @@
-module.exports = function(app){
+var Auth = require('./controllers/auth'); //include the authorization controller
+var User = require('./models/user'); //include the User model
+
+module.exports = function(app){  //sets routes
+	app.post('/signup', Auth.signup);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*module.exports = function(app){
 	app.get('/', function(req, res, next){
 		res.send("HELLO HOMEPAGE");
 	});
@@ -6,4 +28,4 @@ module.exports = function(app){
 	app.get('/signup', function(req, res, next){
 		res.send("Hey folks, Thanks for signing up!");
 	});
-}
+}*/
