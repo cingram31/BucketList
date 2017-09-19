4c6a56da2e3af0d4393d252e3cedcf8c726cc3d3
Christian Ingram <clingram1@aol.com>
2017-09-19 11:10:09 -0400

    Added Passport

--- server/router.js
+++ server/router.js
@@ -1,19 +1,18 @@
 var Auth = require('./controllers/auth'); //include the authorization controller
-var User = require('./models/user'); //include the User model
+var passportService = require('./services/passport'); 
+var passport = require('passport');
+
+var requireAuth = passport.authenticate('jwt', {session: false});
 
 module.exports = function(app){  //sets routes
+
+	app.get('/', requireAuth, function(req, res){
+		res.send('Hello Homepage');
+		//res.send({hi: 'there'});
+	});
+	
 	app.post('/signup', Auth.signup);
 }
-
-
-
-
-
-
-
-
-
-
 
 
 
