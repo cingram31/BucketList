cd1e44eeb9b28947d930626b472e3419fca55871
Christian Ingram <clingram1@aol.com>
2017-09-20 11:26:59 -0400

    Added components pt2

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Component } from 'react';
 import NavBarHeader from './nav';
+import Video from './video/video'
 
 	export default class App extends Component {
 		render() {
 			return (
 				<div>
 					<NavBarHeader />
+					<Video />
 				</div>
-				);
+			);
 		}
 	}
 
