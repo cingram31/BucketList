7a1a36252054862ecd62047693172253c231f74c
Christian Ingram <clingram1@aol.com>
2017-09-21 18:06:54 -0400

    Added Action creator list

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,25 @@
+import axios from 'axios';
+
+export const CREATE_POSTS = 'CREATE_POSTS';
+
+const ROOT_URL = 'http://rest.learncode.academy/api/paul';
+
+export function createPost(props) {
+	const request = axios.post(`${ROOT_URL}/posts`, props);
+	return {
+		type: CREATE_POSTS,
+		payload: request
+	};
+}
+
+
+
+
+
+
+
 //Action Constant Names
-const SELECT_BAND = 'SELECT_BAND';
+/*const SELECT_BAND = 'SELECT_BAND';
 
 export function selectBand(band) {
 	console.log("You have selected:", band.name);
@@ -10,4 +30,4 @@ export function selectBand(band) {
 		type: SELECT_BAND,
 		payload: band
 	};
-}
+}*/
---
+++ client/src/components/list/new-list-item.js
@@ -0,0 +1,16 @@
+import React, { Component, Proptypes } from react;
+import { reduxForm } from 'redux-form';
+import { createPost } from '../../actions/index';
+import { Link } from 'react-router';
+
+class ListItem extends Component {
+	handleFormSubmit(formProps){
+		//call action creator to sign up the user
+			this.props.createPost(formProps);
+	}
+}
+
+export default reduxForm({
+	form: 'PostsNewForm',
+	fields: ['title', 'topic', 'url', 'content']
+}, null, { createPost })(ListItem);
