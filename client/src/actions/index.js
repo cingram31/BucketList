2855b0222a2cc793e0480b9dc159c9121ed38d4d
Christian Ingram <clingram1@aol.com>
2017-09-22 21:01:39 -0400

    Added AuthError

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,11 @@
 import axios from 'axios';
 import { browserHistory } from 'react-router';
+import { 
+	AUTH_USER, 
+	UNAUTH_USER,
+	AUTH_ERROR
+} from './types';
+import authReducer from '../reducers/auth_reducer';
 
 const ROOT_URL = 'http://localhost:3000';
 
@@ -19,13 +25,26 @@ export function signinUser({ email, password }){
 	return function(dispatch){
 		axios.post(`${ROOT_URL}/signin`, {email, password})
 			.then(response => {
+				dispatch({ type: AUTH_USER });
+					console.log(response)
+
+				localStorage.setItem('token', response.data.token);
 				browserHistory.push('/newitem');
 			})
-				.catch(() => {
-			});
-
+				.catch(response => dispatch(authError("Bad login info")));
+			
+		}
 	}
-}
+		
+		
+export function authError(error) {
+	return {
+		type: AUTH_ERROR,
+		payload: error
+		};
+	}
+
+	
 
 
 
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,13 +1,11 @@
 import { combineReducers } from 'redux';
-import BandsReducer from './reducer_bands';
-import SelectedBand from './reducer_selectedband';
-import {reducer as formReducer} from 'redux-form';
-
+import { reducer as formReducer } from 'redux-form';
+import authReducer from './auth_reducer';
 
 	// Define the properties of our Application State here
 	const rootReducer = combineReducers({
 		form: formReducer,
-		//SelectedBand: SelectedBand
+		auth: authReducer
 	});
 
 	export default rootReducer;
