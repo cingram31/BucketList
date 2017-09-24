e133a68e8081e24a678b22dbefca4cb67a94ecd5
Christian Ingram <clingram1@aol.com>
2017-09-24 19:58:24 -0400

    Added updates and fixes

--- server/controllers/bucketlistcontroller.js
+++ server/controllers/bucketlistcontroller.js
@@ -7,7 +7,7 @@ exports.addBucketList = function(req, res, next){
 	var topic =req.body.props.topic;
 	var url = req.body.props.url;
 	var content = req.body.props.content;
-	var specificUser = req.user;
+	var specificUser = req.user._id;
 
 	var bucketList = new BucketList({
 		title: title,
@@ -20,5 +20,65 @@ exports.addBucketList = function(req, res, next){
 	bucketList.save(function(err){
 		if(err) { return next(err); }
 		res.json(bucketList);
+	});
+}
+
+exports.fetchBucketLists = function(req, res) {
+	var specificUser = req.user._id;
+	BucketList.find({specificUser: specificUser})
+	.then(
+		function fetchSuccess(data) {
+			res.json(data);
+		},
+		function fetchError(err) {
+			res.send(500, err.message);
+		}
+	);
+}
+
+exports.fetchBucketList = function(req, res) {
+	var specificBucketList = req.params.id;
+	BucketList.findOne({_id: specificBucketList})
+	.then(
+		function fetchSuccess(data) {
+			res.json(data);
+		},
+		function fetchError(err) {
+			res.send(500, err.message);
+		}
+	);
+}
+exports.deleteBucketList = function(req, res) {
+	var specificBucketList = req.params.id;
+	BucketList.remove({_id: specificBucketList})
+	.then(
+		function deleteSuccess(data) {
+			res.json(data);
+		},
+		function deleteError(err) {
+			res.send(500, err.message);
+		}
+	);
+}
+
+exports.updateBucketList = function(req, res) {
+	var specificBucketList = req.params.id;
+	BucketList.findById(specificBucketList, function(err, bucketlistUpdate) {
+		if (err) {
+			res.status(500, err.message)
+		} else {
+
+			bucketlistUpdate.title = req.body.props.title;
+			bucketlistUpdate.topic = req.body.props.topic;
+			bucketlistUpdate.url = req.body.props.url;
+			bucketlistUpdate.content = req.body.props.content;
+
+			bucketlistUpdate.save(function(err,bucketlist){
+				if (err) {
+					res.status(500, err.message)
+				}
+				res.send(bucketlist);
+			});
+		};
 	});
 }
--- server/router.js
+++ server/router.js
@@ -18,6 +18,10 @@ module.exports = function(app){  //sets routes
 	app.post('/signup', Auth.signup);
 	app.post('/signin', requireSignin, Auth.signin);
 	app.post('/newitem', requireAuth, BucketList.addBucketList);
+	app.get('/items', requireAuth, BucketList.fetchBucketLists);
+	app.get('/items/:id', requireAuth, BucketList.fetchBucketList);
+	app.put('/items/:id', requireAuth, BucketList.updateBucketList);
+	app.delete('/items/:id', requireAuth, BucketList.deleteBucketList);
 }
 
 
